4bfb892ab4c4da8e99019b102e36c5bce93823de
Carlos <carloscga97@gmail.com>
2024-05-01 20:27:28 +0200

    add dagger hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -49,6 +51,11 @@ android {
     }
 }
 
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -66,4 +73,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //DAGGER HILT
+    implementation(libs.hilt.android) //"com.google.dagger:hilt-android:2.46"
+    kapt(libs.hilt.compiler) //"com.google.dagger:hilt-compiler:2.46"
+
+
 }
