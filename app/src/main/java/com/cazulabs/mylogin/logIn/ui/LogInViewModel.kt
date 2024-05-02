f2c212e03acadf08e34fe1ed4df29049c33e0fa2
Carlos <carloscga97@gmail.com>
2024-05-02 21:04:40 +0200

    create logIn viewModel

---
+++ app/src/main/java/com/cazulabs/mylogin/logIn/ui/LogInViewModel.kt
@@ -0,0 +1,23 @@
+package com.cazulabs.mylogin.logIn.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LogInViewModel @Inject constructor(): ViewModel() {
+
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+
+    private val _phone = MutableLiveData<String>()
+    val phone: LiveData<String> = _phone
+
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String> = _password
+
+
+
+}
