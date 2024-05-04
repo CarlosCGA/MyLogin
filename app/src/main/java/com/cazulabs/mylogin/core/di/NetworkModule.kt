361a7fc9e529850caff5d5bbeb90ba963bac7c18
Carlos <carloscga97@gmail.com>
2024-05-04 13:37:24 +0200

    create retrofit provider

---
+++ app/src/main/java/com/cazulabs/mylogin/core/di/NetworkModule.kt
@@ -0,0 +1,24 @@
+package com.cazulabs.mylogin.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://run.mocky.io/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
