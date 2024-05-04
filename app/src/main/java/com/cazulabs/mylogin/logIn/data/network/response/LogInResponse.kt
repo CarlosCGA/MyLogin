c722ab7d96a3afc0c4253ab0faaa6febf19558b8
Carlos <carloscga97@gmail.com>
2024-05-04 13:40:57 +0200

    add logInClient & response

---
+++ app/src/main/java/com/cazulabs/mylogin/logIn/data/network/response/LogInResponse.kt
@@ -0,0 +1,5 @@
+package com.cazulabs.mylogin.logIn.data.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LogInResponse(@SerializedName("success") val success: Boolean)
