be1d33601f81de62caffe884024a7b1d985645c0
Carlos <carloscga97@gmail.com>
2024-05-06 00:27:23 +0200

    add splash screen to routes

--- app/src/main/java/com/cazulabs/mylogin/core/Routes.kt
+++ app/src/main/java/com/cazulabs/mylogin/core/Routes.kt
@@ -2,6 +2,7 @@ package com.cazulabs.mylogin.core
 
 sealed class Routes(val route: String) {
 
+    data object Splash: Routes("Splash")
     data object LogIn: Routes("LogIn")
     data object SignIn: Routes("SignIn")
 
