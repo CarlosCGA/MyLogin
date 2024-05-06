d38d4d69e4ea547d2a5a03b2aa730e45e35ee2f4
Carlos <carloscga97@gmail.com>
2024-05-06 19:25:08 +0200

    copy logInViewModel to signInViewModel

--- app/src/main/java/com/cazulabs/mylogin/signIn/ui/SignInViewModel.kt
+++ app/src/main/java/com/cazulabs/mylogin/signIn/ui/SignInViewModel.kt
@@ -1,12 +1,68 @@
 package com.cazulabs.mylogin.signIn.ui
 
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SignInViewModel @Inject constructor() : ViewModel() {
 
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+
+    private val _phone = MutableLiveData<String>()
+    val phone: LiveData<String> = _phone
+
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String> = _password
+
+    private val _isLogInEnabled = MutableLiveData<Boolean>()
+    val isLogInEnabled: LiveData<Boolean> = _isLogInEnabled
+
+    /**
+     * Update viewModel variables when updated in input textFields
+     */
+    fun onLogInChanged(
+        email: String,
+        phone: String,
+        password: String
+    ) {
+        _email.value = email
+        _phone.value = phone
+        _password.value = password
+
+        enableLogIn(email, phone, password)
+    }
+
+    /**
+     * Conditions of input logIn to enable logIn button
+     */
+    private fun enableLogIn(email: String, phone: String, password: String) {
+        _isLogInEnabled.value =
+            Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                    && Patterns.PHONE.matcher(phone).matches()
+                    && password.isNotBlank()
+                    && password.isNotEmpty()
+    }
+
+    /**
+     * Do logIn method
+     */
+    fun onSignIn() {
+        viewModelScope.launch {
+            /*
+            val result = logInUseCase(email.value!!, password.value!!)
+
+            if(result)
+                Log.i("CARLOS", "GO IN!")
+                */
+        }
+    }
 
 
 }
