01f73e7ab036f14e58c0689eaaf67267b5ea000b
Carlos <carloscga97@gmail.com>
2024-05-06 19:19:06 +0200

    delete logIn viewModel

--- app/src/main/java/com/cazulabs/mylogin/MainActivity.kt
+++ app/src/main/java/com/cazulabs/mylogin/MainActivity.kt
@@ -3,20 +3,16 @@ package com.cazulabs.mylogin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.cazulabs.mylogin.core.navigation.AppNavigation
-import com.cazulabs.mylogin.logIn.ui.LogInViewModel
 import com.cazulabs.mylogin.ui.theme.MyLoginTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val logInViewModel: LogInViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +27,6 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-
-    fun getLogInViewModel(): LogInViewModel {
-        return logInViewModel
     }
 
 }
