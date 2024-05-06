a9dde0fac7c2978a351a4c4be30193be3576d7be
Carlos <carloscga97@gmail.com>
2024-05-06 19:32:32 +0200

    copy logInScreen body component

--- app/src/main/java/com/cazulabs/mylogin/signIn/ui/SignInScreen.kt
+++ app/src/main/java/com/cazulabs/mylogin/signIn/ui/SignInScreen.kt
@@ -5,22 +5,33 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.cazulabs.mylogin.logIn.ui.Email
+import com.cazulabs.mylogin.logIn.ui.Password
+import com.cazulabs.mylogin.logIn.ui.Phone
 
-@Preview(showSystemUi = true)
 @Composable
-fun SignInScreen() {
+fun SignInScreen(signInViewModel: SignInViewModel = hiltViewModel<SignInViewModel>()) {
     Box(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxWidth()) {
             Spacer(modifier = Modifier.weight(0.33F))
             Body(
                 modifier = Modifier
                     .weight(1F)
-                    .align(Alignment.CenterHorizontally)
+                    .align(Alignment.CenterHorizontally),
+                signInViewModel
             )
         }
 
@@ -28,8 +39,64 @@ fun SignInScreen() {
 }
 
 @Composable
-fun Body(modifier: Modifier) {
+fun Body(modifier: Modifier, signInViewModel: SignInViewModel) {
+    val email by signInViewModel.email.observeAsState(initial = "")
+    val phone by signInViewModel.phone.observeAsState(initial = "")
+    val password by signInViewModel.password.observeAsState(initial = "")
+    val isSignInEnabled by signInViewModel.isLogInEnabled.observeAsState(initial = false)
+
     Column(modifier = modifier) {
+        SignInTitle(modifier = Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.size(36.dp))
+
+        Email(email) { newEmail ->
+            signInViewModel.onLogInChanged(newEmail, phone, password)
+        }
+        Spacer(modifier = Modifier.size(8.dp))
+        Phone(phone) { newPhone ->
+            signInViewModel.onLogInChanged(email, newPhone, password)
+        }
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Password(password) { newPassword ->
+            signInViewModel.onLogInChanged(email, phone, newPassword)
+        }
+
+        Spacer(modifier = Modifier.size(24.dp))
+
+        ButtonSignIn(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            isLogInEnabled = isSignInEnabled,
+            signInViewModel = signInViewModel
+        )
+    }
+}
+
+@Composable
+fun ButtonSignIn(modifier: Modifier, isLogInEnabled: Boolean, signInViewModel: SignInViewModel) {
+    Button(
+        modifier = modifier,
+        shape = RoundedCornerShape(6.dp),
+        onClick = { signInViewModel.onSignIn() },
+        enabled = isLogInEnabled
+    ) {
         Text(text = "Sign In")
     }
+}
+
+@Composable
+fun SignInTitle(modifier: Modifier) {
+    Text(
+        modifier = modifier,
+        text = "SignIn",
+        fontWeight = FontWeight.Bold
+    )
+}
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun SignInScreenPreview() {
+    SignInScreen()
 }
