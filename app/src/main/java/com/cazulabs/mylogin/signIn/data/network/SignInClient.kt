179ee1853788e5dcbfd6c5bc12b3043e1cfc2d25
Carlos <carlos.canete@espaciosoa.com>
2024-05-07 09:01:52 +0200

    create signIn interface for endpoints call

---
+++ app/src/main/java/com/cazulabs/mylogin/signIn/data/network/SignInClient.kt
@@ -0,0 +1,12 @@
+package com.cazulabs.mylogin.signIn.data.network
+
+import com.cazulabs.mylogin.signIn.data.network.response.SignInResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface SignInClient {
+
+    @GET("v3/2a203145-d253-42d2-92ab-190630e682ea")
+    suspend fun doSignIn(/*user: String, password: String*/): Response<SignInResponse>
+
+}
