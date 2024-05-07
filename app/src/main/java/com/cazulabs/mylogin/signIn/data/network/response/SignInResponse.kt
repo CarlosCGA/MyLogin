520e2aade0ca772e613391aefe7b8295bb4ef526
Carlos <carlos.canete@espaciosoa.com>
2024-05-07 09:19:32 +0200

    update signIn network response

--- app/src/main/java/com/cazulabs/mylogin/signIn/data/network/response/SignInResponse.kt
+++ app/src/main/java/com/cazulabs/mylogin/signIn/data/network/response/SignInResponse.kt
@@ -2,4 +2,4 @@ package com.cazulabs.mylogin.signIn.data.network.response
 
 import com.google.gson.annotations.SerializedName
 
-data class SignInResponse(@SerializedName("success") val success: String)
+data class SignInResponse(@SerializedName("success") val success: Boolean)
