70c320d13c4a09ae78d115429beaf2227d8d2842
Carlos <carloscga97@gmail.com>
2024-05-11 12:43:57 +0200

    create countries information model

---
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/model/CountriesInformationModel.kt
@@ -0,0 +1,10 @@
+package com.cazulabs.mylogin.countriesInformation.data.model
+
+data class model(
+    val name: String,
+    val countryCode: String,
+    val emoji: String,
+    val unicode: String,
+    val dialCode: String,
+    val imageURL: String,
+)
