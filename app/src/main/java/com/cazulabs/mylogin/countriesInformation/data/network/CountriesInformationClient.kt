d178beb199ac94938007a6af4fdbe05ff6effeea
Carlos <carloscga97@gmail.com>
2024-05-11 11:52:25 +0200

    update endpoint

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/network/CountriesInformationClient.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/network/CountriesInformationClient.kt
@@ -1,11 +1,11 @@
 package com.cazulabs.mylogin.countriesInformation.data.network
 
-import com.cazulabs.mylogin.countriesInformation.data.network.response.CountriesInformationResponse
+import com.cazulabs.mylogin.countriesInformation.data.network.response.CountryInformationResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface CountriesInformationClient {
 
-    @GET("v3/f763e56b-6a5b-4f6d-81c7-c7e30f6f618f")
-    suspend fun getCountriesInformation(): Response<CountriesInformationResponse>
+    @GET("v3/9bfa16f4-b86d-4cee-b324-5479a093d0d9")
+    suspend fun getCountriesInformation(): Response<List<CountryInformationResponse>>
 }
