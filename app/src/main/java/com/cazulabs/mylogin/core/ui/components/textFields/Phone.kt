89f5ef77346b56c589cabe1dd0a6cad59c104ed1
Carlos <carloscga97@gmail.com>
2024-05-12 11:50:52 +0200

    update phone component without prefix

--- app/src/main/java/com/cazulabs/mylogin/core/ui/components/textFields/Phone.kt
+++ app/src/main/java/com/cazulabs/mylogin/core/ui/components/textFields/Phone.kt
@@ -4,19 +4,25 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import com.cazulabs.mylogin.countriesInformation.data.network.response.CountryInformationResponse
 
 @Composable
 fun Phone(
-    phonePrefix: String = "",
-    onPhonePrefixChange: (String) -> Unit = {},
     phone: String,
     onValueChange: (String) -> Unit
 ) {
@@ -27,10 +33,6 @@ fun Phone(
             onValueChange(newPhone)
         },
         singleLine = true,
-        prefix = {
-            if (phonePrefix.isNotEmpty())
-                PhonePrefix(phonePrefix = phonePrefix)
-        },
         label = { Text(text = "Phone") },
         leadingIcon = {
             Icon(imageVector = Icons.Outlined.Phone, contentDescription = "phone")
@@ -39,15 +41,10 @@ fun Phone(
     )
 }
 
-@Composable
-fun PhonePrefix(phonePrefix: String) {
-    Text(modifier = Modifier.clickable { }, text = "+$phonePrefix", color = Color.Gray)
-}
-
 @Preview(showBackground = true)
 @Composable
 fun PhonePreview() {
-    Phone(phonePrefix = "34", phone = "") {
+    Phone(phone = "") {
 
     }
 }
