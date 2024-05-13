cf3d169fe88ff097c6ad9d4b4030d0d7ff3ce93a
Carlos <carloscga97@gmail.com>
2024-05-13 12:13:46 +0200

    update splash screen icon

--- app/src/main/java/com/cazulabs/mylogin/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/cazulabs/mylogin/splashScreen/SplashScreen.kt
@@ -1,18 +1,18 @@
 package com.cazulabs.mylogin.splashScreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.cazulabs.mylogin.R
 import kotlinx.coroutines.delay
 
 @Composable
@@ -34,9 +34,9 @@ fun Splash() {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(
+        Image(
             modifier = Modifier.size(60.dp),
-            imageVector = Icons.Outlined.ShoppingCart,
+            painter = painterResource(id = R.drawable.ic_app),
             contentDescription = "icon"
         )
     }
