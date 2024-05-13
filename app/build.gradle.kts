2f99507286c3729c36506372f4cf66de1cf8f611
Carlos <carloscga97@gmail.com>
2024-05-13 22:40:27 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "v2.0_13052024"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
