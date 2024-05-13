4280476dedf984d4806b16781efab128678e5470
Carlos <carloscga97@gmail.com>
2024-05-13 10:13:09 +0200

    update get countries information return new data model

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationRepository.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationRepository.kt
@@ -1,13 +1,22 @@
 package com.cazulabs.mylogin.countriesInformation.data
 
+import com.cazulabs.mylogin.countriesInformation.data.model.CountryInformationModel
 import com.cazulabs.mylogin.countriesInformation.data.network.CountriesInformationService
-import com.cazulabs.mylogin.countriesInformation.data.network.response.CountryInformationResponse
 import javax.inject.Inject
 
 class CountriesInformationRepository @Inject constructor(private val api: CountriesInformationService) {
 
-    suspend fun getCountriesInformation(): List<CountryInformationResponse> {
-        return api.getCountriesInformation()
+    suspend fun getCountriesInformation(): List<CountryInformationModel> {
+        return api.getCountriesInformation().map { countryResponse ->
+            CountryInformationModel(
+                name = countryResponse.name,
+                countryCode = countryResponse.countryCode,
+                emoji = countryResponse.emoji,
+                unicode = countryResponse.unicode,
+                dialCode = countryResponse.dialCode,
+                imageURL = countryResponse.imageURL
+            )
+        }
     }
 
 }
--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/domain/GetCountriesInformationUseCase.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/domain/GetCountriesInformationUseCase.kt
@@ -1,12 +1,12 @@
 package com.cazulabs.mylogin.countriesInformation.domain
 
 import com.cazulabs.mylogin.countriesInformation.data.CountriesInformationRepository
-import com.cazulabs.mylogin.countriesInformation.data.network.response.CountryInformationResponse
+import com.cazulabs.mylogin.countriesInformation.data.model.CountryInformationModel
 import javax.inject.Inject
 
 class GetCountriesInformationUseCase @Inject constructor(private val repository: CountriesInformationRepository) {
 
-    suspend operator fun invoke(): List<CountryInformationResponse> {
+    suspend operator fun invoke(): List<CountryInformationModel> {
         return repository.getCountriesInformation()
     }
 
