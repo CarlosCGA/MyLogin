e5cb880c382e7af188c1b75510b1cbae5f8321c1
Carlos <carlos.canete@espaciosoa.com>
2024-05-14 09:30:27 +0200

    add resetPassword response

---
+++ app/src/main/java/com/cazulabs/mylogin/resetPassword/data/network/response/ResetPasswordResponse.kt
@@ -0,0 +1,5 @@
+package com.cazulabs.mylogin.resetPassword.data.network.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ResetPasswordResponse(@SerializedName("success") val success: String)
