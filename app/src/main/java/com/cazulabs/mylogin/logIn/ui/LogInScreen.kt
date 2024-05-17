f555bc70535d362e274cf1368f7d0ed0d8d2b1ba
Carlos <carloscga97@gmail.com>
2024-05-17 19:51:29 +0200

    clean imports

--- app/src/main/java/com/cazulabs/mylogin/logIn/ui/LogInScreen.kt
+++ app/src/main/java/com/cazulabs/mylogin/logIn/ui/LogInScreen.kt
@@ -16,11 +16,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.cazulabs.mylogin.core.navigation.Routes
 import com.cazulabs.mylogin.core.ui.components.textFields.Email
 import com.cazulabs.mylogin.core.ui.components.textFields.Password
