9f051150e19d977b57dda4301776bf0f20f53b97
Carlos <carloscga97@gmail.com>
2024-05-18 17:10:36 +0200

    add queries to get info

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationDAO.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationDAO.kt
@@ -3,9 +3,22 @@ package com.cazulabs.mylogin.countriesInformation.data
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
 
 @Dao
 interface CountriesInformationDAO {
+
+
+    @Query("SELECT * FROM CountryInformationEntity")
+    fun getCountriesInformation(): List<CountryInformationEntity>
+
+    /*
+        @Query("SELECT * FROM CountryInformationEntity")
+        fun getCountriesInformation(): Flow<List<CountryInformationEntity>>
+     */
+
+    @Query("SELECT id, name, emoji, dialCode FROM CountryInformationEntity")
+    fun getCountriesPhonePrefix(): List<CountryInformationEntity>
 
     @Insert
     suspend fun insertAll(countriesInformation: List<CountryInformationEntity>)
