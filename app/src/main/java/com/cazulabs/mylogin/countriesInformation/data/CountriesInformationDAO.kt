3a43cba75abe79ecb7c509ae9d7954ec9d07e5d5
Carlos <carloscga97@gmail.com>
2024-05-20 17:53:56 +0200

    update and comment methods (testing)

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationDAO.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationDAO.kt
@@ -1,29 +1,31 @@
 package com.cazulabs.mylogin.countriesInformation.data
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CountriesInformationDAO {
 
 
-    @Query("SELECT * FROM CountryInformationEntity")
-    fun getCountriesInformation(): List<CountryInformationEntity>
+    @Query("SELECT * FROM countries")
+    fun getCountriesInformation(): Flow<List<CountryInformationEntity>>
 
     /*
         @Query("SELECT * FROM CountryInformationEntity")
         fun getCountriesInformation(): Flow<List<CountryInformationEntity>>
      */
 
-    @Query("SELECT id, name, emoji, dialCode FROM CountryInformationEntity")
+    @Query("SELECT id, name, emoji, dial_code FROM countries")
     fun getCountriesPhonePrefix(): List<CountryInformationEntity>
 
     @Insert
     suspend fun insertAll(countriesInformation: List<CountryInformationEntity>)
 
-    @Delete
+    /*
+    @DeleteTable()
     suspend fun deleteAll()
+    */
 
 }
--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationRepository.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/CountriesInformationRepository.kt
@@ -3,6 +3,9 @@ package com.cazulabs.mylogin.countriesInformation.data
 import com.cazulabs.mylogin.countriesInformation.data.model.CountryInformationModel
 import com.cazulabs.mylogin.countriesInformation.data.model.CountryPhonePrefixModel
 import com.cazulabs.mylogin.countriesInformation.data.network.CountriesInformationService
+import com.cazulabs.mylogin.countriesInformation.data.network.response.CountryInformationResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -12,28 +15,32 @@ class CountriesInformationRepository @Inject constructor(
     private val api: CountriesInformationService
 ) {
 
-    var countriesInformation: List<CountryInformationModel> =
-        countriesInformationDAO.getCountriesInformation()
-            .map {
+    val countriesInformation: Flow<List<CountryInformationModel>> =
+        countriesInformationDAO.getCountriesInformation().map { items ->
+            items.map {
                 CountryInformationModel(
-                    it.name,
-                    it.countryCode,
-                    it.emoji,
-                    it.unicode,
-                    it.dialCode,
-                    it.imageURL
+                    name = it.name ?: "",
+                    countryCode = it.countryCode ?: "",
+                    emoji = it.emoji ?: "",
+                    unicode = it.unicode ?: "",
+                    dialCode = it.dialCode ?: "",
+                    imageURL = it.imageURL ?: ""
                 )
             }
+        }
 
+
+    /*
     var countriesPhonePrefix: List<CountryPhonePrefixModel> =
         countriesInformationDAO.getCountriesPhonePrefix()
             .map {
                 CountryPhonePrefixModel(
-                    it.name,
-                    it.emoji,
-                    it.dialCode
+                    it.name ?: "",
+                    it.emoji ?: "",
+                    it.dialCode ?: ""
                 )
             }
+            */
 
     /*
     val countriesInformation: Flow<List<CountryInformationModel>> =
@@ -47,10 +54,21 @@ class CountriesInformationRepository @Inject constructor(
         })
     }
 
+    /*
     suspend fun deleteAll() {
         countriesInformationDAO.deleteAll()
     }
+    */
 
+    suspend fun getCountriesInformation(): List<CountryInformationModel> {
+        return api.getCountriesInformation().map { it.toCountryInformationModel() }
+    }
+
+    suspend fun getCountriesPhonePrefix(): List<CountryPhonePrefixModel> {
+        return api.getCountriesInformation()
+            .map { it.toCountryInformationPhonePrefix() }
+    }
+    /*
     suspend fun getCountriesInformation(): List<CountryInformationModel> {
         if (countriesInformation.isEmpty()) {
             countriesInformation = api.getCountriesInformation().map { countryResponse ->
@@ -84,17 +102,37 @@ class CountriesInformationRepository @Inject constructor(
             emptyList()
         }
     }
+    */
 
 }
 
 fun CountryInformationModel.toData(): CountryInformationEntity {
     return CountryInformationEntity(
-        null,
+        0,
         this.name,
         this.countryCode,
         this.emoji,
         this.unicode,
         this.dialCode,
         this.imageURL
+    )
+}
+
+fun CountryInformationResponse.toCountryInformationModel(): CountryInformationModel {
+    return CountryInformationModel(
+        this.name,
+        this.countryCode,
+        this.emoji,
+        this.unicode,
+        this.dialCode,
+        this.imageURL
+    )
+}
+
+fun CountryInformationResponse.toCountryInformationPhonePrefix(): CountryPhonePrefixModel {
+    return CountryPhonePrefixModel(
+        this.name,
+        this.emoji,
+        this.dialCode ?: ""
     )
 }
