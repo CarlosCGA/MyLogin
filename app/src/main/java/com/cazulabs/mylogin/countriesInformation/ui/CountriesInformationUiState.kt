9537e98eecda647fdc009a8b1988a78c58291f96
Carlos <carloscga97@gmail.com>
2024-05-22 22:50:44 +0200

    update param name

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/ui/CountriesInformationUiState.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/ui/CountriesInformationUiState.kt
@@ -3,7 +3,8 @@ package com.cazulabs.mylogin.countriesInformation.ui
 import com.cazulabs.mylogin.countriesInformation.data.model.CountryInformationModel
 
 sealed interface CountriesInformationUiState {
-    data object Loading: CountriesInformationUiState
-    data class Error(val throwable: Throwable): CountriesInformationUiState
-    data class Success(val tasks:List<CountryInformationModel>): CountriesInformationUiState
+    data object Loading : CountriesInformationUiState
+    data class Error(val throwable: Throwable) : CountriesInformationUiState
+    data class Success(val countriesInformation: List<CountryInformationModel>) :
+        CountriesInformationUiState
 }
