0269ee3e8df36167321cd467317d2f3881171009
Carlos <carloscga97@gmail.com>
2024-05-22 23:04:53 +0200

    rename

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/domain/InsertCountriesInformationUseCase.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/domain/InsertCountriesInformationUseCase.kt
@@ -3,7 +3,7 @@ package com.cazulabs.mylogin.countriesInformation.domain
 import com.cazulabs.mylogin.countriesInformation.data.CountriesInformationRepository
 import javax.inject.Inject
 
-class GetAndInsertUseCase @Inject constructor(private val repository: CountriesInformationRepository) {
+class InsertCountriesInformationUseCase @Inject constructor(private val repository: CountriesInformationRepository) {
 
     suspend operator fun invoke() {
         repository.getAndInsertInLocal()
