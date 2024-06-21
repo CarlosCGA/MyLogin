4e0bde0b6e8cb57b17c78c009a64d8b6662f272b
Carlos <carlos.canete@espaciosoa.com>
2024-06-22 01:54:27 +0200

    re-structure network module

--- app/src/main/java/com/cazulabs/mylogin/resetPassword/data/di/NetworkModule.kt
+++ app/src/main/java/com/cazulabs/mylogin/resetPassword/data/di/NetworkModule.kt
@@ -1,4 +1,21 @@
 package com.cazulabs.mylogin.resetPassword.data.di
 
+import com.cazulabs.mylogin.resetPassword.data.network.ResetPasswordClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideResetPasswordClient(retrofit: Retrofit): ResetPasswordClient {
+        return retrofit.create(ResetPasswordClient::class.java)
+    }
+
 }
