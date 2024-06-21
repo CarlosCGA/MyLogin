fa6cffe26620af4d7118075790984311fdfdf881
Carlos <carlos.canete@espaciosoa.com>
2024-06-22 01:54:43 +0200

    re-structure network module

--- app/src/main/java/com/cazulabs/mylogin/signIn/data/di/NetworkModule.kt
+++ app/src/main/java/com/cazulabs/mylogin/signIn/data/di/NetworkModule.kt
@@ -1,4 +1,21 @@
 package com.cazulabs.mylogin.signIn.data.di
 
+import com.cazulabs.mylogin.signIn.data.network.SignInClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideSignInClient(retrofit: Retrofit): SignInClient {
+        return retrofit.create(SignInClient::class.java)
+    }
+
 }
