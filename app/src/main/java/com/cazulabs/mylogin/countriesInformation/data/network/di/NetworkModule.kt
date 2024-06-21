eef6563d19bfceafc33d5bbb58951e45860367ab
Carlos <carlos.canete@espaciosoa.com>
2024-06-22 01:54:11 +0200

    re-structure network module

--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/network/di/NetworkModule.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/network/di/NetworkModule.kt
@@ -1,7 +1,21 @@
 package com.cazulabs.mylogin.countriesInformation.data.network.di
 
+import com.cazulabs.mylogin.countriesInformation.data.network.CountriesInformationClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
 
-
+    @Singleton
+    @Provides
+    fun provideCountriesInformationClient(retrofit: Retrofit): CountriesInformationClient {
+        return retrofit.create(CountriesInformationClient::class.java)
+    }
 
 }
