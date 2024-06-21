7d986ecf83251ecc288f5146a648c1a865766f13
Carlos <carlos.canete@espaciosoa.com>
2024-06-22 01:54:00 +0200

    re-structure network module

--- app/src/main/java/com/cazulabs/mylogin/checkUser/data/di/NetworkModule.kt
+++ app/src/main/java/com/cazulabs/mylogin/checkUser/data/di/NetworkModule.kt
@@ -1,4 +1,21 @@
 package com.cazulabs.mylogin.checkUser.data.di
 
+import com.cazulabs.mylogin.checkUser.data.network.CheckUserClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideCheckUserClient(retrofit: Retrofit): CheckUserClient {
+        return retrofit.create(CheckUserClient::class.java)
+    }
+
 }
