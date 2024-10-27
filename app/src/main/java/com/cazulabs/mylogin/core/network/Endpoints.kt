76abd8c76f1c330be620184045f737f5f9d497a7
Carlos <carloscga97@gmail.com>
2024-10-27 13:29:19 +0100

    Update API request endpoints

--- app/src/main/java/com/cazulabs/mylogin/core/network/Endpoints.kt
+++ app/src/main/java/com/cazulabs/mylogin/core/network/Endpoints.kt
@@ -1,6 +1,6 @@
 package com.cazulabs.mylogin.core.network
 
 object Endpoints {
-    const val DEFAULT = "v3/2a203145-d253-42d2-92ab-190630e682ea"
-
+    const val DEFAULT = "v3/0f67d544-a2ee-4c71-9cd5-31a36929b3b6"
+    const val GET_COUNTRIES_INFORMATION = "v3/acf2100e-7242-4a89-9481-959d7356e5e8"
 }
--- app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/network/CountriesInformationClient.kt
+++ app/src/main/java/com/cazulabs/mylogin/countriesInformation/data/network/CountriesInformationClient.kt
@@ -1,11 +1,12 @@
 package com.cazulabs.mylogin.countriesInformation.data.network
 
+import com.cazulabs.mylogin.core.network.Endpoints
 import com.cazulabs.mylogin.countriesInformation.data.network.response.CountryInformationResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface CountriesInformationClient {
 
-    @GET("v3/9bfa16f4-b86d-4cee-b324-5479a093d0d9")
+    @GET(Endpoints.GET_COUNTRIES_INFORMATION)
     suspend fun getCountriesInformation(): Response<List<CountryInformationResponse>>
 }
